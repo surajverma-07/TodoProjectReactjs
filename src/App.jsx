c44e87264e214b4007def9d48f0e386c6b3056c9
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-03-16 20:27:14 +0530

    functinalities are written of todo app in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import { useState } from 'react'
-
+import { TodoProvider } from './context/TodoContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState({})
+  const addTodo = (todomsg) =>{
+    setTodos((prev)=>[{id:Date.now(),todomsg:todomsg,completed:false},...prev])
+  }
+  const updateTodo = (id,todomsg) =>{
+    setTodos((prev)=>prev.map((prevtodo)=>(prevtodo.id === id ? (todomsg) : (prevtodo) )))
+  }
+  const deleteTodo = (id) =>{
+     setTodos((prev)=>prev.filter((prevtodo)=> (prevtodo.id !== id )))
+  }
+  const toggle = (id) =>{
+    setTodos((prev)=>prev.map((prevtodo)=>(prevtodo.id === id ?{...prevtodo,completed: !prevtodo.completed}:(prevtodo))))
+  }
 
   return (
-    <>
-        <div className='text-center bg-blue-400'>Suraj Kumar Verma </div>        
-    </>
+   
+   <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggle}}>
+     <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                   
+                       
+                    </div>
+                </div>
+            </div>
+   </TodoProvider>
   )
 }
 
--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,5 +1,21 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const TodoContext = createContext({
+    todos :[
+       {
+        id:1,
+        todomsg:"message",
+        completed:false,
+       }
+    ],
+    addTodo : (todomsg) => {},
+    updateTodo : (id,todomsg) => {},
+    deleteTodo : (id) => {},
+    toggle : (id,) => {},
     
 })
+
+export const useTodo = () =>{
+    return useContext(TodoContext);
+}
+export const TodoProvider = TodoContext.Provider
